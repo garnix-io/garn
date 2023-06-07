6256f7c09466d762e088adc902ea12d4770f19b8
Julian K. Arni <jkarni@gmail.com>
2023-06-07 20:32:46 +0200

    Golden spec function

--- test/spec/Garner/CodeGenSpec.hs
+++ test/spec/Garner/CodeGenSpec.hs
@@ -5,12 +5,12 @@ module Garner.CodeGenSpec (spec) where
 import Data.String.Interpolate (i)
 import Garner.CodeGen
 import Test.Hspec
-import Test.Hspec.Golden (defaultGolden)
+import Test.Hspec.Golden (Golden, defaultGolden)
 
 spec :: Spec
 spec = do
   describe "fromToplevelDerivation" $ do
-    it "extracts top-level derivations" $ do
+    golden "extracts top-level derivations" $ \check -> do
       output <-
         fromToplevelDerivation
           "pkgs"
@@ -23,7 +23,18 @@ spec = do
               };
             }
           |]
-      return $ defaultGolden "hello" output
+      return $ check output
     it "ignores things that are not derivations" pending
     it "only generates valid typescript" pending
     it "generates nice JSDoc comments" pending
+
+golden :: Example a => String -> ((String -> Golden String) -> a) -> SpecWith (Arg a)
+golden description test =
+  it description $
+    test (defaultGolden $ fmap cleanChar description)
+  where
+    cleanChar ' ' = '_'
+    cleanChar '/' = '_'
+    cleanChar '\'' = '_'
+    cleanChar '"' = '_'
+    cleanChar x = x
