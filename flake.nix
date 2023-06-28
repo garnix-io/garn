d47da6bc252dd3aa04850d6b99c0eb33114f898c
Julian K. Arni <jkarni@gmail.com>
2023-06-28 10:40:01 +0200

    Make a garner flake app

--- flake.nix
+++ flake.nix
@@ -16,6 +16,12 @@
       in
       {
         lib = pkgs.lib;
+        app = {
+          default = {
+            type = "program";
+            program = "${self.packages.${system}.default}/bin/garner";
+          };
+        };
         packages = {
           default =
             let
