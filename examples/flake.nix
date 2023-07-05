8c5cc13163ab847aaee28b51d9f5559d46bdc1d7
Greg Pfeil <greg@technomadic.org>
2023-07-05 12:43:51 -1000

    Generate formatted flakes
    Garner now produces flakes with a `formatter` output (currently hardcoded), and
    uses `nix fmt` to apply that formatter to the output, avoiding the need to
    maintain formatting during generation.
    
    That said, this does reformat the TS flake template to match the hardcoded
    formatter (for easier eyeballing), but it also removes some attempts at padding
    elements that are spliced into the template.
    
    Fixes #17.

--- examples/flake.nix
+++ examples/flake.nix
@@ -1,40 +1,54 @@
 {
-        inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-        outputs = { self, nixpkgs } :
-           let
-             systems = ["x86_64-linux"];
-             forAllSystems = nixpkgs.lib.genAttrs systems;
-           in
-             {
-                packages = forAllSystems (system:
-                  let pkgs = import "${nixpkgs}" {
-                    config.allowUnfree = true;
-                    inherit system;
-                  };
-                  in {
-                      
-    haskellExecutable = (pkgs.haskell.packages.ghc94.callCabal2nix "mkHaskell-test" ./. { } ) // { meta.mainProgram = "garnerTest"; };
-    hello = pkgs.hello;
-                  });
-                devShells = forAllSystems (system:
-                  let pkgs = import "${nixpkgs}" {
-                    config.allowUnfree = true;
-                    inherit system;
-                  };
-                  in {
-                      
-     haskellExecutable =  let expr = self.packages.${system}.haskellExecutable;
-      in if self.packages.${system}.haskellExecutable ? env
-         then self.packages.${system}.haskellExecutable.env
-         else pkgs.mkShell({
-           inputsFrom = [ self.packages.${system}.haskellExecutable ];
-         });
-     hello =  let expr = self.packages.${system}.hello;
-      in if self.packages.${system}.hello ? env
-         then self.packages.${system}.hello.env
-         else pkgs.mkShell({
-           inputsFrom = [ self.packages.${system}.hello ];
-         });
-                  });
-             };
-     }
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        {
+          haskellExecutable = (pkgs.haskell.packages.ghc94.callCabal2nix "mkHaskell-test" ./. { }) // { meta.mainProgram = "garnerTest"; };
+          hello = pkgs.hello;
+        });
+      devShells = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        {
+          haskellExecutable =
+            let expr = self.packages.${system}.haskellExecutable;
+            in if self.packages.${system}.haskellExecutable ? env
+            then self.packages.${system}.haskellExecutable.env
+            else
+              pkgs.mkShell ({
+                inputsFrom = [ self.packages.${system}.haskellExecutable ];
+              });
+          hello =
+            let expr = self.packages.${system}.hello;
+            in if self.packages.${system}.hello ? env
+            then self.packages.${system}.hello.env
+            else
+              pkgs.mkShell ({
+                inputsFrom = [ self.packages.${system}.hello ];
+              });
+        });
+      formatter = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        pkgs.nixpkgs-fmt);
+    };
+}
--- ts/runner.ts
+++ ts/runner.ts
@@ -4,40 +4,52 @@ const encoder = new TextEncoder();
 
 const formatFlake = (config: Record<string, Package>): string => {
   const packages = Object.entries(config).reduce(
-    (acc, [name, pkg]) => acc + `\n    ${name} = ${pkg.nixExpression};`,
+    (acc, [name, pkg]) => acc + `${name} = ${pkg.nixExpression};`,
     ""
   );
   const shells = Object.entries(config).reduce((acc, [name, pkg]) => {
     const pkgAttr = `self.packages.\${system}.${name}`;
     const env = pkg.envExpression(pkgAttr);
-    return acc + `\n     ${name} = ${env};`;
+    return acc + `${name} = ${env};`;
   }, "");
   return `{
-        inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-        outputs = { self, nixpkgs } :
-           let
-             systems = ["x86_64-linux"];
-             forAllSystems = nixpkgs.lib.genAttrs systems;
-           in
-             {
-                packages = forAllSystems (system:
-                  let pkgs = import "\${nixpkgs}" {
-                    config.allowUnfree = true;
-                    inherit system;
-                  };
-                  in {
-                      ${packages}
-                  });
-                devShells = forAllSystems (system:
-                  let pkgs = import "\${nixpkgs}" {
-                    config.allowUnfree = true;
-                    inherit system;
-                  };
-                  in {
-                      ${shells}
-                  });
-             };
-     }`;
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+    outputs = { self, nixpkgs }:
+      let
+        systems = [ "x86_64-linux" ];
+        forAllSystems = nixpkgs.lib.genAttrs systems;
+      in
+      {
+        packages = forAllSystems (system:
+          let
+            pkgs = import "\${nixpkgs}" {
+              config.allowUnfree = true;
+              inherit system;
+            };
+          in
+          {
+            ${packages}
+          });
+        devShells = forAllSystems (system:
+          let
+            pkgs = import "\${nixpkgs}" {
+              config.allowUnfree = true;
+              inherit system;
+            };
+          in
+          {
+            ${shells}
+          });
+        formatter = forAllSystems (system:
+          let
+            pkgs = import "\${nixpkgs}" {
+              config.allowUnfree = true;
+              inherit system;
+            };
+          in
+          pkgs.nixpkgs-fmt);
+      };
+  }`;
 };
 
 export const writeFlake = (config: Record<string, Package>) => {
