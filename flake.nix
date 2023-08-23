7ae3e3367d266f3f52da50610b5cd7cecae54658
Greg Pfeil <greg@technomadic.org>
2023-08-23 15:31:12 -0600

    Provide ts library without fileserver

--- flake.nix
+++ flake.nix
@@ -61,6 +61,54 @@
                   doCheck = false;
                 }
               );
+          nixpkgs-ts = pkgs.stdenv.mkDerivation {
+            name = "nixpkgs.ts";
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.cacert
+              pkgs.nix
+              self.packages.${system}.garner
+            ];
+
+            LC_ALL = "C.UTF-8";
+
+            buildPhase = ''
+              export HOME="$PWD/fake-home"
+              mkdir -p "$HOME"
+              codegen
+            '';
+
+            installPhase = ''
+              mkdir -p "$out/share/garner"
+              cp ts/nixpkgs.ts "$out/share/garner"
+            '';
+
+            outputHash = "TgnOyeVN2wpsM2vIYwk3jd0Gx14G6lY2XTfJ8+M4PGA=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+          };
+          typescript = pkgs.stdenv.mkDerivation {
+            name = "garner-typescript";
+            src = ./ts;
+
+            nativeBuildInputs = [
+              self.packages.${system}.nixpkgs-ts
+            ];
+
+            buildPhase = ''
+              cp --no-preserve=mode --recursive "$src" ./
+              ## This is for garner itself, not users
+              rm runner.ts
+            '';
+
+            installPhase = ''
+              mkdir -p "$out/share"
+              cp --recursive ./ "$out/share/garner"
+              cp ${self.packages.${system}.nixpkgs-ts}/share/garner/* \
+                 "$out/share/garner"
+            '';
+          };
         };
         devShells = {
           default = pkgs.mkShell {
@@ -90,6 +138,15 @@
           ##     environment will leak, and isolation will be lost.
           barren = pkgs.mkShell {
             nativeBuildInputs = [ self.packages.${system}.garner ];
+            shellHook = ''
+              find . -name 'garner.ts' -exec \
+                bash -c \
+                  'source "${pkgs.stdenv}/setup" && \
+                   substituteInPlace "$0" \
+                     --replace \
+                     "http://localhost:8777/" \
+                     "${self.packages.${system}.typescript}/share/garner/"' {} \;
+            '';
           };
         };
         checks =
