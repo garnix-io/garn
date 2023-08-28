328f108ffdfd0c1a9cee7bff677be5ef71fe84ff
Greg Pfeil <gmpfeil@garnix.io>
2023-08-28 12:31:46 -1000

    Add bashInteractive to default devShell

--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
               builtins.attrValues self.checks.${system}
               ++ builtins.attrValues self.packages.${system};
             nativeBuildInputs = with pkgs; [
+              bashInteractive
               ghcid
               cabal-install
               (ourHaskell.ghc.withPackages (p:
