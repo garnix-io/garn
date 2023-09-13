bc50b5d6ad5da6a46bb1fb1cd8ac5b3112c3756d
Sönke Hahn <soenkehahn@gmail.com>
2023-09-13 12:40:24 +0200

    Format flake.nix in example

--- test-examples/frontend-create-react-app/flake.nix
+++ test-examples/frontend-create-react-app/flake.nix
@@ -1,48 +1,49 @@
 {
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/841889913dfd06a70ffb39f603e29e46f45f0c1a";
-    outputs = { self, nixpkgs }:
-      let
-        systems = [ "x86_64-linux" ];
-        forAllSystems = nixpkgs.lib.genAttrs systems;
-      in
-      {
-        packages = forAllSystems (system:
-          let
-            pkgs = import "${nixpkgs}" {
-              config.allowUnfree = true;
-              inherit system;
-            };
-          in
-          {
-            main = 
-    pkgs.stdenv.mkDerivation {
-      name = "frontend";
-      buildInputs = [ pkgs.nodejs-18_x ];
-    }
-  ;
-          });
-        devShells = forAllSystems (system:
-          let
-            pkgs = import "${nixpkgs}" {
-              config.allowUnfree = true;
-              inherit system;
-            };
-          in
-          {
-            main =  let expr = self.packages.${system}.main;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/841889913dfd06a70ffb39f603e29e46f45f0c1a";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
         in
-          (if expr ? env
+        {
+          main =
+            pkgs.stdenv.mkDerivation {
+              name = "frontend";
+              buildInputs = [ pkgs.nodejs-18_x ];
+            }
+          ;
+        });
+      devShells = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        {
+          main =
+            let expr = self.packages.${system}.main;
+            in
+            (if expr ? env
             then expr.env
-            else pkgs.mkShell {inputsFrom = [ expr ];}
-          );
-          });
-        formatter = forAllSystems (system:
-          let
-            pkgs = import "${nixpkgs}" {
-              config.allowUnfree = true;
-              inherit system;
-            };
-          in
-          pkgs.nixpkgs-fmt);
-      };
-  }
+            else pkgs.mkShell { inputsFrom = [ expr ]; }
+            );
+        });
+      formatter = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        pkgs.nixpkgs-fmt);
+    };
+}
