69fecc5330887651aef9d37d7c0490a8d0ec4bd2
Sönke Hahn <soenkehahn@gmail.com>
2023-09-14 12:41:38 +0200

    Fix examples

--- examples/npm-frontend/flake.nix
+++ examples/npm-frontend/flake.nix
@@ -1,6 +1,12 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/841889913dfd06a70ffb39f603e29e46f45f0c1a";
-  outputs = { self, nixpkgs }:
+
+  inputs.npmlock2nix-repo = {
+    url = "github:nix-community/npmlock2nix?rev=9197bbf397d76059a76310523d45df10d2e4ca81";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, npmlock2nix-repo }:
     let
       systems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -15,10 +21,24 @@
         in
         {
           frontend =
-            pkgs.stdenv.mkDerivation {
-              name = "frontend";
-              buildInputs = [ pkgs.nodejs-18_x ];
-            }
+            let
+              npmlock2nix = import npmlock2nix-repo {
+                inherit pkgs;
+              };
+              pkgs = import "${nixpkgs}" {
+                config.permittedInsecurePackages = [ ];
+                inherit system;
+              };
+            in
+            npmlock2nix.v2.build
+              {
+                src = ./.;
+                buildCommands = [ "" "mkdir $out" ];
+                installPhase = "true";
+                node_modules_attrs = {
+                  nodejs = pkgs.nodejs-18_x;
+                };
+              }
           ;
         });
       devShells = forAllSystems (system:
