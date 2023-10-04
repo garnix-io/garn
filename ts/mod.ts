b818cb02e7a7ce953a74b4145f35fb0ba348267e
Sönke Hahn <soenkehahn@gmail.com>
2023-10-04 18:47:06 -0400

    Tweak mod.ts exports

--- ts/mod.ts
+++ ts/mod.ts
@@ -1,7 +1,9 @@
 export { type OldPackage } from "./base.ts";
 export { shell } from "./executable.ts";
+export { type Package, mkPackage } from "./package.ts";
+export { type Project, mkProject } from "./project.ts";
+
+// languages
 export * as go from "./go.ts";
 export * as haskell from "./haskell.ts";
-export { type Package } from "./package.ts";
-export { type Project } from "./project.ts";
 export * as typescript from "./typescript.ts";
