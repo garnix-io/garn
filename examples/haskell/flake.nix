822c1457b2642419a2818e4e98b0e57537777885
Sönke Hahn <soenkehahn@gmail.com>
2023-10-04 15:30:49 -0400

    Update haskell example flake file

--- examples/haskell/flake.nix
+++ examples/haskell/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/841889913dfd06a70ffb39f603e29e46f45f0c1a";
-  outputs = { self, nixpkgs }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
     let
       systems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -87,6 +88,41 @@
             else pkgs.mkShell { inputsFrom = [ expr ]; }
             )
           ;
+        });
+      apps = forAllSystems (system:
+        let
+          pkgs = import "${nixpkgs}" { inherit system; };
+        in
+        {
+          haskellExecutable = {
+            type = "app";
+            program = "${
+    (pkgs.haskell.packages.ghc94.callCabal2nix
+      "garner-pkg"
+      
+  (let
+    lib = pkgs.lib;
+    lastSafe = list :
+      if lib.lists.length list == 0
+        then null
+        else lib.lists.last list;
+  in
+  builtins.path
+    {
+      path = ./.;
+      filter = path: type:
+        let
+          fileName = lastSafe (lib.strings.splitString "/" path);
+        in
+         fileName != "flake.nix";
+    })
+
+      { })
+      // {
+        meta.mainProgram = "garnerTest";
+      }
+  }/bin/garnerTest";
+          };
         });
     };
 }
