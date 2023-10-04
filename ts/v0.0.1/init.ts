51b43823ec0a373eedd44f5707e678c539e68828
Julian K. Arni <jkarni@gmail.com>
2023-10-04 17:37:57 +0200

    Release v0.0.1

---
+++ ts/v0.0.1/init.ts
@@ -0,0 +1,25 @@
+import * as haskell from "./haskell.ts";
+
+let imports = "";
+let body = "";
+
+const initializers = haskell.initializers;
+
+console.error("[garner] Creating a garner.ts file");
+
+for (const init of initializers) {
+  const result = init();
+  switch (result.tag) {
+    case "UnexpectedError":
+      console.error("[garner] " + result.reason);
+      break;
+    case "ShouldNotRun":
+      break;
+    case "ShouldRun":
+      imports += result.imports;
+      body += result.makeTarget();
+      break;
+  }
+}
+
+Deno.writeTextFileSync("garner.ts", imports + "\n\n" + body);
