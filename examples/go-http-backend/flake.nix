82fdb38bd7ad27533340784c9738ca7e0f89de44
Alex David <git@alexd.me>
2023-10-04 15:05:33 -0700

    Fix tests

--- examples/go-http-backend/flake.nix
+++ examples/go-http-backend/flake.nix
@@ -1,7 +1,11 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/841889913dfd06a70ffb39f603e29e46f45f0c1a";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.npmlock2nix-repo = {
+    url = "github:nix-community/npmlock2nix?rev=9197bbf397d76059a76310523d45df10d2e4ca81";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, flake-utils, npmlock2nix-repo }:
     let
       systems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -54,13 +58,13 @@
         {
           server = {
             type = "app";
-            program = "${
+            program = "${pkgs.writeScriptBin "executable" "${
     pkgs.buildGoModule {
       name = "go-project";
       src = ./.;
       vendorHash = null;
     }
-  }/bin/server";
+  }/bin/server"}/bin/executable";
           };
         });
     };
