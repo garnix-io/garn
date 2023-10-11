988ef015f888530c4b2d3554d6dda7bef29fb123
Sönke Hahn <soenkehahn@gmail.com>
2023-10-10 23:26:54 -0400

    Move check tests out

--- test/spec/GarnSpec.hs
+++ test/spec/GarnSpec.hs
@@ -3,7 +3,7 @@
 
 module GarnSpec where
 
-import Control.Monad (forM_, unless)
+import Control.Monad (unless)
 import Data.List (sort)
 import Data.String.Conversions (cs)
 import Data.String.Interpolate (i)
@@ -111,96 +111,6 @@ spec = do
           writeHaskellProject repoDir
           _ <- runGarn ["run", "foo"] "" repoDir Nothing
           readFile "./unformatted.nix" `shouldReturn` unformattedNix
-
-      describe "check" $ do
-        it "runs manually added checks" $ do
-          writeHaskellProject repoDir
-          writeFile
-            "Main.hs"
-            [i|
-              main :: IO ()
-              main = return ()
-
-              f :: [Int] -> [Int]
-              f list = map (+ 1) list
-            |]
-          writeFile
-            "garn.ts"
-            [i|
-              import * as garn from "#{repoDir}/ts/mod.ts"
-
-              const haskellBase = garn.haskell.mkHaskell({
-                description: "mkHaskell-test",
-                executable: "garn-test",
-                compiler: "ghc94",
-                src: "."
-              }).withDevTools([garn.mkPackage(`pkgs.hlint`)]);
-
-              export const haskell = {
-                ...haskellBase,
-                hlint: haskellBase.check`hlint *.hs`,
-              };
-            |]
-          output <- runGarn ["check", "haskell"] "" repoDir Nothing
-          stderr output `shouldContain` "Warning: Eta reduce"
-        it "runs checks on source directories that ignore the flake.nix file" $ do
-          writeHaskellProject repoDir
-          writeFile
-            "garn.ts"
-            [i|
-              import * as garn from "#{repoDir}/ts/mod.ts"
-
-              const haskellBase = garn.haskell.mkHaskell({
-                description: "mkHaskell-test",
-                executable: "garn-test",
-                compiler: "ghc94",
-                src: "."
-              });
-
-              export const haskell = {
-                ...haskellBase,
-                hlint: haskellBase.check`
-                  ls
-                  false
-                `,
-              };
-            |]
-          output <- runGarn ["check", "haskell"] "" repoDir Nothing
-          stderr output `shouldNotContain` "flake.nix"
-        describe "exit-codes" $ do
-          let testCases =
-                [ ("passing", "true", ExitSuccess),
-                  ("failing", "false", ExitFailure 1),
-                  ("unknownCommand", "does-not-exist", ExitFailure 1),
-                  ("failureBeforeOtherCommands", "false; true", ExitFailure 1),
-                  ("negated", "! true", ExitFailure 1),
-                  ("pipefail", "false | true", ExitFailure 1)
-                ]
-          forM_ testCases $ \(checkName, check :: String, expectedExitCode) -> do
-            it ("reports exit-codes correctly for check '" <> checkName <> "'") $ do
-              writeHaskellProject repoDir
-              writeFile
-                "garn.ts"
-                [i|
-                  import * as garn from "#{repoDir}/ts/mod.ts"
-
-                  const haskellBase = garn.haskell.mkHaskell({
-                    description: "mkHaskell-test",
-                    executable: "garn-test",
-                    compiler: "ghc94",
-                    src: "."
-                  });
-
-                  export const haskell = {
-                    ...haskellBase,
-                    check: haskellBase.check`#{check}`,
-                  };
-                |]
-              result <- runGarn ["check", "haskell"] "" repoDir Nothing
-              putStrLn $ stdout result
-              putStrLn $ stderr result
-              stderr result `shouldNotContain` "Invalid argument"
-              exitCode result `shouldBe` expectedExitCode
 
     -- TODO: Golden tests currently can’t be integrated with the other test cases
     --       because stackbuilders/hspec-golden#40. The case below shows the
