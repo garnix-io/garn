6e9d8de6f21b5dbae50b802d13d93efc2a7c5475
Sönke Hahn <soenkehahn@gmail.com>
2023-10-11 14:42:51 -0400

    Add `addCheck` function to `Project` (#191)
    Co-authored-by: Julian Arni <jkarni@gmail.com>

--- examples/haskell/garn.ts
+++ examples/haskell/garn.ts
@@ -1,16 +1,12 @@
 import * as garn from "http://localhost:8777/mod.ts";
 import * as nixpkgs from "http://localhost:8777/nixpkgs.ts";
 
-const base = garn.haskell
+export const helloFromHaskell = garn.haskell
   .mkHaskell({
     description: "My haskell executable",
     executable: "helloFromHaskell",
     compiler: "ghc94",
     src: ".",
   })
-  .withDevTools([nixpkgs.hlint]);
-
-export const helloFromHaskell: garn.Project & { hlint: garn.Check } = {
-  ...base,
-  hlint: base.check`hlint *.hs`,
-};
+  .withDevTools([nixpkgs.hlint])
+  .addCheck("hlint")`hlint *.hs`;
