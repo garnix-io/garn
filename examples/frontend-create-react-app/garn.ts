2254b3e5da418034693406eb38b87d10c8bb1924
Sönke Hahn <soenkehahn@gmail.com>
2023-10-11 14:47:00 -0400

    Rename typescript module to javascript

--- examples/frontend-create-react-app/garn.ts
+++ examples/frontend-create-react-app/garn.ts
@@ -1,6 +1,6 @@
 import * as garn from "http://localhost:8777/mod.ts";
 
-export const main = garn.typescript.mkNpmFrontend({
+export const main = garn.javascript.mkNpmFrontend({
   description: "frontend test app created by create-react-app",
   src: ".",
   nodeVersion: "18",
