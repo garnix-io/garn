97feeb52bfd73ef66cb228a2ba9fd97a7e901f5d
Alex David <git@alexd.me>
2023-10-11 16:49:19 -0700

    Add monorepo example

---
+++ examples/monorepo/garn.ts
@@ -0,0 +1,22 @@
+import * as garn from "http://localhost:8777/mod.ts";
+
+export const backend = garn.go.mkGoProject({
+  description: "example backend server in go",
+  moduleName: "go-http-backend",
+  src: "backend",
+});
+
+export const yarnFrontend = garn.javascript.mkYarnFrontend({
+  description: "my nice yarn project",
+  src: "frontend-yarn",
+  nodeVersion: "18",
+  testCommand: "yarn mocha",
+  serverStartCommand: "yarn start",
+});
+
+export const npmFrontend = garn.javascript.mkNpmFrontend({
+  description: "frontend test app created by create-react-app",
+  src: "frontend-npm",
+  nodeVersion: "18",
+  testCommand: "npm run test -- --watchAll=false",
+});
