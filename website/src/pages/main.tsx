420fa4bd1024004b9ce09a3f1cf42ec61f8d47da
Alex David <alexdavid@users.noreply.github.com>
2023-10-18 12:21:46 -0700

    Add GitHub and doc links to website (#259)
    Co-authored-by: Sönke Hahn <soenkehahn@gmail.com>

--- website/src/pages/main.tsx
+++ website/src/pages/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DropdownMenu } from "../components/Dropdown";
+import { GithubLink } from "../components/GithubLink";
 import { Tooltip } from "../components/Hovernote";
 import { Typography } from "../components/Typography";
 import { Asciinema } from "../components/Asciinema";
@@ -60,6 +61,7 @@ export const Main: React.FC = () => {
             </Link>
           </Typography>
         </div>
+        <GithubLink />
       </header>
 
       <article>
