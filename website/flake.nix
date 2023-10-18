bd1eba08efd687e012fbb9e81c2e6a91da565b57
Alex David <git@alexd.me>
2023-10-17 19:55:47 -0700

    Add typescript-language-server to website devtools

--- website/flake.nix
+++ website/flake.nix
@@ -20,15 +20,17 @@
           };
         in
         {
-          default_pkg =
+          "default_pkg" =
             let
               npmlock2nix = import npmlock2nix-repo {
                 inherit pkgs;
               };
-              pkgs = import "${nixpkgs}" {
-                config.permittedInsecurePackages = [ ];
-                inherit system;
-              };
+              pkgs =
+                import "${nixpkgs}" {
+                  config.permittedInsecurePackages = [ ];
+                  inherit system;
+                }
+              ;
             in
             npmlock2nix.v2.build
               {
@@ -65,7 +67,8 @@
                 };
               }
           ;
-        });
+        }
+      );
       checks = forAllSystems (system:
         let
           pkgs = import "${nixpkgs}" {
@@ -73,7 +76,8 @@
             inherit system;
           };
         in
-        { });
+        { }
+      );
       devShells = forAllSystems (system:
         let
           pkgs = import "${nixpkgs}" {
@@ -82,53 +86,60 @@
           };
         in
         {
-          default =
-            let
-              npmlock2nix = import npmlock2nix-repo {
-                inherit pkgs;
-              };
-            in
-            npmlock2nix.v2.shell {
-              src =
-                (
-                  let
-                    lib = pkgs.lib;
-                    lastSafe = list:
-                      if lib.lists.length list == 0
-                      then null
-                      else lib.lists.last list;
-                  in
-                  builtins.path
-                    {
-                      path = ./.;
-                      name = "source";
-                      filter = path: type:
-                        let
-                          fileName = lastSafe (lib.strings.splitString "/" path);
-                        in
-                        fileName != "flake.nix" &&
-                        fileName != "garn.ts";
-                    }
-                )
-              ;
-              node_modules_mode = "copy";
-              node_modules_attrs = {
-                nodejs = pkgs.nodejs-18_x;
-              };
-            }
+          "default" =
+            (
+              let
+                npmlock2nix = import npmlock2nix-repo {
+                  inherit pkgs;
+                };
+              in
+              npmlock2nix.v2.shell {
+                src =
+                  (
+                    let
+                      lib = pkgs.lib;
+                      lastSafe = list:
+                        if lib.lists.length list == 0
+                        then null
+                        else lib.lists.last list;
+                    in
+                    builtins.path
+                      {
+                        path = ./.;
+                        name = "source";
+                        filter = path: type:
+                          let
+                            fileName = lastSafe (lib.strings.splitString "/" path);
+                          in
+                          fileName != "flake.nix" &&
+                          fileName != "garn.ts";
+                      }
+                  )
+                ;
+                node_modules_mode = "copy";
+                node_modules_attrs = {
+                  nodejs = pkgs.nodejs-18_x;
+                };
+              }
+            ).overrideAttrs (finalAttrs: previousAttrs: {
+              nativeBuildInputs =
+                previousAttrs.nativeBuildInputs
+                ++
+                [ pkgs.nodePackages.typescript-language-server ];
+            })
           ;
-        });
+        }
+      );
       apps = forAllSystems (system:
         let
           pkgs = import "${nixpkgs}" { inherit system; };
         in
         {
-
-          default = {
-            type = "app";
-            program = "${
-        let
-          dev = 
+          "default" = {
+            "type" = "app";
+            "program" = "${
+      let
+        dev = 
       let
         npmlock2nix = import npmlock2nix-repo {
           inherit pkgs;
@@ -161,20 +172,20 @@
         };
       }
     ;
-          shell = "cd . && npm run dev";
-          buildPath = pkgs.runCommand "build-inputs-path" {
-            inherit (dev) buildInputs nativeBuildInputs;
-          } "echo $PATH > $out";
-        in
-        pkgs.writeScript "shell-env"  ''
-          #!${pkgs.bash}/bin/bash
-          export PATH=$(cat ${buildPath}):$PATH
-          ${dev.shellHook}
-          ${shell} "$@"
-        ''
-      }";
+        shell = "cd . && npm run dev";
+        buildPath = pkgs.runCommand "build-inputs-path" {
+          inherit (dev) buildInputs nativeBuildInputs;
+        } "echo $PATH > $out";
+      in
+      pkgs.writeScript "shell-env"  ''
+        #!${pkgs.bash}/bin/bash
+        export PATH=$(cat ${buildPath}):$PATH
+        ${dev.shellHook}
+        ${shell} "$@"
+      ''
+    }";
           };
-
-        });
+        }
+      );
     };
 }
--- website/garn.ts
+++ website/garn.ts
@@ -1,8 +1,11 @@
 import * as garn from "../ts/mod.ts";
+import { nixRaw } from "../ts/nix.ts";
 
 export default garn.javascript.mkNpmProject({
   description: "The garn.io website",
   nodeVersion: "18",
   src: ".",
   startCommand: "npm run dev",
-});
+}).withDevTools([
+  garn.mkPackage(nixRaw('pkgs.nodePackages.typescript-language-server')),
+]);
