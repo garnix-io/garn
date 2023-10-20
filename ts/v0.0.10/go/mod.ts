7d2372fdccae733af8c616a8e3ceeb1b28bb2bc3
Sönke Hahn <soenkehahn@gmail.com>
2023-10-20 15:32:52 -0400

    Release v0.0.10

---
+++ ts/v0.0.10/go/mod.ts
@@ -0,0 +1,89 @@
+import { packageToEnvironment, shell } from "../environment.ts";
+import { NixExpression, nixRaw, nixStrLit } from "../nix.ts";
+import { mkPackage, Package } from "../package.ts";
+import { mkProject, Project } from "../project.ts";
+import * as path from "https://deno.land/std@0.202.0/path/mod.ts";
+import { getDotGarnProjectDir } from "../internal/garn_dir.ts";
+import { nixSource } from "../internal/utils.ts";
+import { GOMOD2NIX_REPO } from "./consts.ts";
+
+const getGoModNixToml = (src: string): NixExpression => {
+  const gen = new Deno.Command("nix", {
+    args: [
+      "run",
+      GOMOD2NIX_REPO,
+      "--",
+      "--dir",
+      src,
+      "--outdir",
+      getDotGarnProjectDir(src),
+    ],
+  }).outputSync();
+  if (!gen.success) {
+    throw Error(
+      [
+        "Failed to generate gomod2nix.toml:",
+        "",
+        "Stdout:",
+        new TextDecoder().decode(gen.stdout),
+        "",
+        "Stderr:",
+        new TextDecoder().decode(gen.stderr),
+      ].join("\n")
+    );
+  }
+  return nixStrLit(
+    Deno.readTextFileSync(
+      path.join(getDotGarnProjectDir(src), "gomod2nix.toml")
+    )
+  );
+};
+
+const GO_VERSION_TO_NIXPKG_NAME = {
+  "1.18": "go_1_18",
+  "1.19": "go_1_19",
+  "1.20": "go_1_20",
+};
+
+/**
+ * Creates a go-based garn Project.
+ */
+export function mkGoProject(args: {
+  description: string;
+  src: string;
+  goVersion?: keyof typeof GO_VERSION_TO_NIXPKG_NAME;
+}): Project & {
+  pkg: Package;
+} {
+  const pkg = mkPackage(
+    nixRaw`
+      let
+        gomod2nix = gomod2nix-repo.legacyPackages.\${system};
+        gomod2nix-toml = pkgs.writeText "gomod2nix-toml" ${getGoModNixToml(
+          args.src
+        )};
+      in
+        gomod2nix.buildGoApplication {
+          pname = "go-package";
+          version = "0.1";
+          go = pkgs.${nixRaw(
+            GO_VERSION_TO_NIXPKG_NAME[args.goVersion ?? "1.20"]
+          )};
+          src = ${nixSource(args.src)};
+          modules = gomod2nix-toml;
+        }
+    `
+  );
+
+  return mkProject(
+    {
+      description: args.description,
+      defaultEnvironment: packageToEnvironment(pkg, args.src).withDevTools([
+        mkPackage(nixRaw`pkgs.gopls`),
+      ]),
+    },
+    {
+      pkg,
+    }
+  );
+}
---
+++ ts/v0.0.10/haskell/initializers.ts
@@ -0,0 +1,95 @@
+import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import * as fs from "https://deno.land/std@0.201.0/fs/mod.ts";
+import outdent from "https://deno.land/x/outdent@v0.8.0/mod.ts";
+import { Initializer } from "../base.ts";
+
+// Currently only works if there's a single cabal file, in the current directory
+const mkHaskellProjectInitializer: Initializer = () => {
+  const cabalFiles: fs.WalkEntry[] = [...fs.expandGlobSync("*.cabal")];
+  if (cabalFiles.length === 0) {
+    return { tag: "ShouldNotRun" };
+  }
+  if (cabalFiles.length > 1) {
+    return {
+      tag: "UnexpectedError",
+      reason: "More than one cabal files found",
+    };
+  }
+  const cmd = new Deno.Command("cabal2json", {
+    args: [cabalFiles[0].path],
+  });
+  const decoder = new TextDecoder();
+  const jsonParseResult = cmd.outputSync();
+  if (jsonParseResult.code !== 0) {
+    return {
+      tag: "UnexpectedError",
+      reason: "Found but could not parse cabal file",
+    };
+  }
+  const parsedCabal = JSON.parse(decoder.decode(jsonParseResult.stdout));
+
+  return {
+    tag: "ShouldRun",
+    makeTarget: () =>
+      outdent`
+      export const ${parsedCabal.name} = garn.haskell.mkHaskellProject({
+        description: "${parsedCabal.synopsis || parsedCabal.description || ""}",
+        executable: "",
+        compiler: "ghc94",
+        src: "."
+      })`,
+  };
+};
+
+export const initializers = [mkHaskellProjectInitializer];
+
+// Tests
+
+Deno.test("Initializer does not run when no cabal file is present", () => {
+  const tempDir = Deno.makeTempDirSync();
+  Deno.chdir(tempDir);
+  const result = mkHaskellProjectInitializer();
+  assertEquals(result.tag, "ShouldNotRun");
+});
+
+Deno.test("Initializer errors if the cabal file is unparseable", () => {
+  const tempDir = Deno.makeTempDirSync();
+  Deno.chdir(tempDir);
+  Deno.writeTextFileSync(
+    "./foo.cabal",
+    `
+    name: foo
+  `
+  );
+  const result = mkHaskellProjectInitializer();
+  assertEquals(result.tag, "UnexpectedError");
+  if (result.tag === "UnexpectedError") {
+    assertEquals(result.reason, "Found but could not parse cabal file");
+  }
+});
+
+Deno.test("Initializer returns a simple string if a cabal file exists", () => {
+  const tempDir = Deno.makeTempDirSync();
+  Deno.chdir(tempDir);
+  Deno.writeTextFileSync(
+    "./foo.cabal",
+    `
+    name: foo
+    version: 0.0.1
+  `
+  );
+  const result = mkHaskellProjectInitializer();
+  assertEquals(result.tag, "ShouldRun");
+  if (result.tag === "ShouldRun") {
+    assertEquals(
+      result.makeTarget(),
+      outdent`
+          export const foo = garn.haskell.mkHaskellProject({
+            description: "",
+            executable: "",
+            compiler: "ghc94",
+            src: "."
+          })`
+    );
+  }
+});
