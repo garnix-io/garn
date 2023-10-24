f0349620615c623723f572d1b454cfa344eaeb73
Sönke Hahn <soenkehahn@gmail.com>
2023-10-24 19:18:06 -0400

    Better readable output of `dbg`

--- src/Garn/Utils.hs
+++ src/Garn/Utils.hs
@@ -1,6 +1,8 @@
 module Garn.Utils where
 
+import Data.String.Conversions (cs)
 import Debug.Trace (trace)
+import Text.Pretty.Simple (pShow)
 
 dbg :: (Show a) => a -> a
-dbg a = trace (show a) a
+dbg a = trace (cs $ pShow a) a
