22fe1a3610f48d7326e347856f2767861c799310
Julian Arni <jkarni@gmail.com>
2023-10-26 19:58:43 +0200

    Fix typos (#301)
    Co-authored-by: Sönke Hahn <soenkehahn@gmail.com>

--- website/src/pages/main.tsx
+++ website/src/pages/main.tsx
@@ -89,8 +89,10 @@ export const Info: React.FC = () => {
       <section className="lede">
           <h1>garn, at your service</h1>
         <div className="garn-description">
-          <b>garn</b> is a build tool and environment manager. It can partly or wholly replace manual README instructions, containerization technologies such as docker, package manager such as apt/brew/&c, and command runners such as just.
-        </div>
+          <b>garn</b> is a build tool and environment manager. It can partly or
+          wholly replace manual README instructions, containerization
+          technologies such as docker, package managers such as apt/brew/&c, and
+          command runners such as just.</div>
         <figure>
           <div className="filename">garn.ts</div>
           <div className="garn-ts">
