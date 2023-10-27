a1c997bf3cecafba6f43960a0e1151ac3b7241c4
Sönke Hahn <soenkehahn@gmail.com>
2023-10-27 12:56:04 -0400

    Apply formatting

--- website/src/components/Asciinema.tsx
+++ website/src/components/Asciinema.tsx
@@ -2,26 +2,22 @@ import React, { useEffect, useRef } from "react";
 import * as AsciinemaPlayer from "asciinema-player";
 
 type AsciinemaProps = {
-  src: string
-  rows?: number
-}
+  src: string;
+  rows?: number;
+};
 
 export const Asciinema: React.FC<AsciinemaProps> = ({ src, rows }) => {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    AsciinemaPlayer.create(
-      src,
-      ref.current,
-      {
-        autoplay: true,
-        loop: true,
-        theme: "garn",
-        terminalFontFamily: "Space Mono",
-        controls: false,
-        rows: rows || 10,
-        cols: 80,
-      }
-    );
+    AsciinemaPlayer.create(src, ref.current, {
+      autoplay: true,
+      loop: true,
+      theme: "garn",
+      terminalFontFamily: "Space Mono",
+      controls: false,
+      rows: rows || 10,
+      cols: 80,
+    });
   });
   return (
     <div className="App">
