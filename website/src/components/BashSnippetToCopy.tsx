a496201ddd7377daa47cded958d5abebe198be74
Alex David <alexdavid@users.noreply.github.com>
2023-11-03 14:20:11 -0700

    Improve copy button (#350)
    * Prevents user select of copy button
    * Prevents wrapping of copy button outside of box on narrow screens
    * Uses `<button>` instead of `<span>` for copy button
    * Restores "copy" text after a few seconds when clicking copy
    
    Resolves #346

--- website/src/components/BashSnippetToCopy.tsx
+++ website/src/components/BashSnippetToCopy.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export const BashSnippetToCopy = (props: { snippet: string }) => {
   const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => setCopied(false), 2000);
+      return () => clearTimeout(timeout);
+    }
+  }, [copied]);
   return (
-    <div className="bash">
-      {props.snippet}
+    <div className="bash-snippet-to-copy">
+      <code>{props.snippet}</code>
       <CopyToClipboard text={props.snippet} onCopy={() => setCopied(true)}>
-        <span className="copy-button">{copied ? "Copied" : "Copy"}</span>
+        <button role="button">{copied ? "Copied" : "Copy"}</button>
       </CopyToClipboard>
     </div>
   );
