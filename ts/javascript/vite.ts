077e5ede198a7d75617fde97a926e8858f57e395
Sönke Hahn <soenkehahn@gmail.com>
2023-11-15 10:16:33 -0500

    Add docstring for vite plugin

--- ts/javascript/vite.ts
+++ ts/javascript/vite.ts
@@ -2,6 +2,35 @@ import { Executable } from "../executable.ts";
 import { Package } from "../package.ts";
 import { Plugin } from "../project.ts";
 
+/**
+ * `Plugin` for [vite](https://vitejs.dev/) projects. This will add to your
+ * `Project`:
+ *
+ * - a `Package` called `build` that will bundle your vite project,
+ * - an `Executable` called `dev` that will run your dev server,
+ * - an `Executable` called `preview` that will run a server to preview your
+ *   production build.
+ *
+ * This plugin requires a field `node_modules` of type `Package` to work.
+ * Usually that is created with `garn.javascript.mkNpmProject`.
+ *
+ * Here's an example:
+ *
+ * ```typescript
+ * import * as garn from "https://garn.io/ts/v0.0.15/mod.ts";
+ *
+ * export const frontend = garn.javascript
+ *   .mkNpmProject({
+ *     description: "An NPM project",
+ *     src: ".",
+ *     nodeVersion: "18",
+ *   })
+ *   .add(garn.javascript.vite);
+ * ```
+ *
+ * After running `garn run frontend.build` you can then access your built
+ * frontend bundle in `./result`.
+ */
 export const plugin: Plugin<
   { build: Package; dev: Executable; preview: Executable },
   { node_modules: Package }
