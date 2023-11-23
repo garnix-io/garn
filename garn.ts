a9dcdd235f12cbc8025eb0c949db71eb0e1000cb
Julian K. Arni <jkarni@gmail.com>
2023-11-23 23:01:03 +0100

    Remove stray garn.ts file

--- garn.ts
+++
@@ -1,9 +0,0 @@
-import * as garn from "https://garn.io/ts/v0.0.15/mod.ts";
-import * as pkgs from "https://garn.io/ts/v0.0.15/nixpkgs.ts";
-
-export const garn = garn.haskell.mkHaskellProject({
-  description: "",
-  compiler: "ghc94",
-  executables: ["garn","codegen"],
-  src: "."
-})
