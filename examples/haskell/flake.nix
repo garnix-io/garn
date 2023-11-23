50ed4c1e93e2882c5b6b45e0ea63f0cd8e905242
Sönke Hahn <soenkehahn@gmail.com>
2023-11-22 19:05:32 -0500

    Update flake files

--- examples/haskell/flake.nix
+++ examples/haskell/flake.nix
@@ -210,11 +210,11 @@
          fileName != "garn.ts";
     })
         { });
-    in
-      (if expr ? env
-        then expr.env
-        else pkgs.mkShell { inputsFrom = [ expr ]; }
-      );
+        in
+          (if expr ? env
+            then expr.env
+            else pkgs.mkShell { inputsFrom = [ expr ]; }
+          );
         shell = "${(pkgs.haskell.packages.ghc94.callCabal2nix
         "garn-pkg"
         (let
