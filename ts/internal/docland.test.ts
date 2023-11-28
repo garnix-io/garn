95974e118fe45b32e08344860b4a595c41f76eb8
Alex David <alexdavid@users.noreply.github.com>
2023-11-28 12:50:17 -0800

    Update ts/internal/docland.test.ts
    Co-authored-by: Sönke Hahn <soenkehahn@gmail.com>

--- ts/internal/docland.test.ts
+++ ts/internal/docland.test.ts
@@ -1,5 +1,5 @@
 // This is the dependency file of the repo deployed at doc.deno.land. We import
-// deno_doc's `doc` here to check if our modules are parsable by the verison of
+// deno_doc's `doc` here to check if our modules are parsable by the version of
 // deno_doc used by docland.
 import { doc } from "https://raw.githubusercontent.com/denoland/docland/main/deps.ts";
 import { describe, it } from "https://deno.land/std@0.206.0/testing/bdd.ts";
