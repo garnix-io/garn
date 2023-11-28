01dedb19aeb3cf25cc3cd3ddc52c74b390d4d600
Julian K. Arni <jkarni@gmail.com>
2023-11-28 12:36:39 +0100

    Website update from commit 54ada149903349f94f4f084fce5bbe70b60e7845

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ else
 fi
 
 echo "installing 'garn'..."
-nix --extra-experimental-features 'nix-command flakes' profile install -L "github:garnix-io/garn/v0.0.16"
+nix --extra-experimental-features 'nix-command flakes' profile install -L "github:garnix-io/garn/v0.0.17"
 echo "testing 'garn' installation..."
 garn --help
 
