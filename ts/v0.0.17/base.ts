dc32fd83ca2983db1a66ad0ab32cfe863f9b9426
Julian K. Arni <jkarni@gmail.com>
2023-11-28 12:29:31 +0100

    Undo v0.0.17

--- ts/v0.0.17/base.ts
+++
@@ -1,6 +0,0 @@
-export type Initializer = (
-  directory: string,
-) =>
-  | { tag: "ShouldRun"; imports?: Array<string>; makeTarget: () => string }
-  | { tag: "ShouldNotRun" }
-  | { tag: "UnexpectedError"; reason: string };
