841512b6295c168e65f557c23001c8c2dcab1900
DavHau <hsngrmpf+github@gmail.com>
2023-11-29 14:11:05 +0700

    add python devShell + codegen for interpreters

--- ts/python/mod.test.ts
+++ ts/python/mod.test.ts
@@ -1,47 +1,57 @@
-import { describe, it } from "https://deno.land/std@0.206.0/testing/bdd.ts";
+import {
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.206.0/testing/bdd.ts";
 import * as garn from "./mod.ts";
-import { buildPackage, runCommand } from "../testUtils.ts";
+import { buildPackage, runCommand, runInDevShell } from "../testUtils.ts";
 import { assert } from "https://deno.land/std@0.206.0/assert/mod.ts";
 import { existsSync } from "https://deno.land/std@0.201.0/fs/exists.ts";
 import { assertStringIncludes } from "https://deno.land/std@0.206.0/assert/assert_string_includes.ts";
 import outdent from "https://deno.land/x/outdent@v0.8.0/mod.ts";
+import * as pythonInterpreters from "../internal/nixpkgs/pythonInterpreters/mod.ts";
+
+const pyprojectToml = outdent`
+  [build-system]
+  requires = ["setuptools"]
+  build-backend = "setuptools.build_meta"
+
+  [project]
+  name = "my_package"
+  version = "0.0.0"
+
+  dependencies = [
+    "requests"
+  ]
+
+  [project.scripts]
+  my-script = "my_package:main"
+`;
+
+const myPackagePy = outdent`
+  import requests
+
+  def main():
+      print(f"using requests version {requests.__version__}")
+      print("Hello, world!")
+
+  if __name__ == "__main__":
+      main()
+`;
 
 describe("mkPythonProject", () => {
-  it("allows building a python project and running scripts from it", () => {
-    const tempDir = Deno.makeTempDirSync({
-      prefix: "garn-mkPythonProject-test",
-    });
-    const pyprojectToml = outdent`
-      [build-system]
-      requires = ["setuptools"]
-      build-backend = "setuptools.build_meta"
-
-      [project]
-      name = "my_package"
-      version = "0.0.0"
-
-      dependencies = [
-        "requests"
-      ]
-
-      [project.scripts]
-      my-script = "my_package:main"
-    `;
-    const myPackagePy = outdent`
-      import requests
-
-      def main():
-          print(f"using requests version {requests.__version__}")
-          print("Hello, world!")
-
-      if __name__ == "__main__":
-          main()
-    `;
+  let tempDir: string;
+  beforeEach(() => {
+    tempDir = Deno.makeTempDirSync({ prefix: "garn-mkPythonProject-test" });
     Deno.writeTextFileSync(`${tempDir}/pyproject.toml`, pyprojectToml);
     Deno.writeTextFileSync(`${tempDir}/my_package.py`, myPackagePy);
+  });
+
+  it("allows building a python project and running scripts from it", () => {
     const project = garn.mkPythonProjectSimple({
       src: "./.",
       description: "A python project",
+      pythonInterpreter: pythonInterpreters.python310,
     });
     const output = buildPackage(project.package, { dir: tempDir });
     assert(existsSync(`${output}/bin/my-script`));
@@ -50,5 +60,17 @@ describe("mkPythonProject", () => {
       new Deno.Command(`${output}/bin/my-script`, {}),
     );
     assertStringIncludes(myScriptOutput.stdout, "Hello, world!");
+  });
+
+  it("generates a working devShell containing the python deps", () => {
+    const project = garn.mkPythonProjectSimple({
+      src: "./.",
+      description: "A python project",
+      pythonInterpreter: pythonInterpreters.python310,
+    });
+    runInDevShell(project.devShell, {
+      dir: tempDir,
+      cmd: "python -c 'import requests; print(requests.__version__)'",
+    });
   });
 });
--- ts/python/mod.ts
+++ ts/python/mod.ts
@@ -3,7 +3,6 @@ import { mkPackage, Package } from "../package.ts";
 import { mkProject, Project } from "../project.ts";
 import { nixSource } from "../internal/utils.ts";
 import { nixRaw } from "../nix.ts";
-
 export { plugin as vite } from "../javascript/vite.ts";
 
 /**
@@ -12,43 +11,89 @@ export { plugin as vite } from "../javascript/vite.ts";
 export function mkPythonProjectSimple(args: {
   description: string;
   src: string;
+  pythonInterpreter: Package;
 }): Project & {
   package: Package;
-  // devShell: Environment;
+  devShell: Environment;
 } {
+  const python = args.pythonInterpreter.nixExpression;
+  const pythonDependencies = nixRaw`
+    let
+      python = ${python};
+      src = ${nixSource(args.src)};
+      pyproject = builtins.fromTOML
+        (builtins.readFile "\${src}/pyproject.toml");
+      dependencies = pyproject.project.dependencies;
+      toPackages = map (dep: python.pkgs.\${dep});
+    in
+      toPackages dependencies
+  `;
   const pkg: Package = mkPackage(
     nixRaw`
       let
         lib = pkgs.lib;
-        python = pkgs.python3;
+        python = ${python};
         src = ${nixSource(args.src)};
         pyproject = builtins.fromTOML
           (builtins.readFile "\${src}/pyproject.toml");
         dependencies = pyproject.project.dependencies;
         toPackages = map (dep: python.pkgs.\${dep});
       in
-        pkgs.python3.pkgs.buildPythonPackage {
+        python.pkgs.buildPythonPackage {
           pname = pyproject.project.name;
           version = pyproject.project.version;
           src = ${nixSource(args.src)};
           format = "pyproject";
           buildInputs = toPackages pyproject.build-system.requires;
-          propagatedBuildInputs = toPackages dependencies;
+          propagatedBuildInputs = ${pythonDependencies};
         }
     `,
     "package",
   );
   const devShell: Environment = mkEnvironment({
-    nixExpression: nixRaw`echo TODO`,
+    nixExpression: nixRaw`
+      let
+        python = ${python};
+        pythonDependencies = ${pythonDependencies};
+        pythonWithDeps = python.withPackages (ps:
+          pythonDependencies
+          ++ [python.pkgs.pip]
+        );
+        shellHook = ''
+          tmp_path=$(realpath ./.pythonenv)
+          mkdir -p "$tmp_path"
+
+          repo_root=$(realpath .)
+          mkdir -p "$tmp_path/python/\${python.sitePackages}"
+
+          # Install the package in editable mode
+          # This allows executing scripts from within the dev-shell using the current
+          # version of the code and its dependencies.
+          \${pythonWithDeps.interpreter} -m pip install \
+            --quiet \
+            --disable-pip-version-check \
+            --no-index \
+            --no-build-isolation \
+            --prefix "$tmp_path/python" \
+            --editable $repo_root
+
+          export PATH="$tmp_path/python/bin:$PATH"
+          export PYTHONPATH="$repo_root:$tmp_path/python/\${pythonWithDeps.sitePackages}:"
+        '';
+      in
+        pythonWithDeps.env.overrideAttrs (old: {
+          shellHook = old.shellHook or "" + "\\n" + shellHook;
+        })
+    `,
   });
   return mkProject(
     {
       description: args.description,
-      // defaultEnvironment: devShell,
+      defaultEnvironment: devShell,
     },
     {
       package: pkg,
-      // devShell,
+      devShell,
     },
   );
 }
