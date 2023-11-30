f099216546514f839c823111bfaabba51d13d813
Eric Liu <eyliubit@gmail.com>
2023-11-30 14:40:49 -0800

    fix lint

--- website2/src/app/layout.tsx
+++ website2/src/app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const MatterSQMono = localFont({
-  src: '../../public/fonts/MatterSQMonoTRIAL-Light.woff',
-})
+  src: "../../public/fonts/MatterSQMonoTRIAL-Light.woff",
+});
 
 export const metadata: Metadata = {
-  title: 'garn',
-  description: 'A build tool and environment manager',
-}
+  title: "garn",
+  description: "A build tool and environment manager",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={MatterSQMono.className}>{children}</body>
     </html>
-  )
+  );
 }
