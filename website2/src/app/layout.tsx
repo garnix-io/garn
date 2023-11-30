e58c42f1c153a4db7469969591d936aed2733087
Eric Liu <eyliubit@gmail.com>
2023-11-30 14:42:04 -0800

    use anon functions

--- website2/src/app/layout.tsx
+++ website2/src/app/layout.tsx
@@ -11,14 +11,12 @@ export const metadata: Metadata = {
   description: "A build tool and environment manager",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={MatterSQMono.className}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- website2/src/app/page.tsx
+++ website2/src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
+const Home = () => {
   return <main />;
-}
+};
+
+export default Home;
