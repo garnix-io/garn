10621ff8964d0a11407047af7061e6128bf1402f
Eric Liu <eyliubit@gmail.com>
2023-12-11 18:53:04 -0800

    add documentation page header

---
+++ website2/src/app/documentation/page.tsx
@@ -0,0 +1,15 @@
+import { DocumentationHeader } from "@/components/documentationPage/documentationHeader";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+
+const Documentation = () => {
+  return (
+    <main>
+      <Header />
+      <DocumentationHeader />
+      <Footer />
+    </main>
+  );
+};
+
+export default Documentation;
---
+++ website2/src/components/documentationPage/documentationHeader/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@/components/text";
+import styles from "./styles.module.css";
+
+export const DocumentationHeader = () => {
+  return (
+    <section className={styles.container}>
+      <Text type="h1">Documentation</Text>
+    </section>
+  );
+};
