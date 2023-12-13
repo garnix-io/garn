299bc06d026cb95d32bd7a697209a722d6aeedde
Eric Liu <eyliubit@gmail.com>
2023-12-13 15:22:20 -0800

    remove redirect and rename getting started

--- website2/next.config.js
+++ website2/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  async redirects() {
-    return [
-      {
-        source: '/docs/getting_started',
-        destination: '/docs/getting-started',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
