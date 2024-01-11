34428a3ae2ff1038a2bd938b6db490b2d370c6d0
Sönke Hahn <soenkehahn@gmail.com>
2024-01-11 16:11:46 -0500

    Serve the Berlin font in the same way

--- website2/src/utils/fonts/index.ts
+++ website2/src/utils/fonts/index.ts
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
 import fonts from "./fonts.module.css";
 
 export const MatterSQMono = {
   className: fonts.MatterSQMono,
 };
 
-export const Berlin = localFont({
-  src: [
-    {
-      path: "./BerlinTypeWeb-Regular.woff",
-      weight: "400",
-    },
-  ],
-});
+export const Berlin = {
+  className: fonts.Berlin,
+};
